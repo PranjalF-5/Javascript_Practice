59ffa8a6a675a73cb8ffa5c9efa2933a458a9ee1
Pranjal Mayank <120745757+PranjalF-5@users.noreply.github.com>
2024-05-31 10:26:06 +0000

    datatypes learnt

---
+++ 01_basics/datatype.js
@@ -0,0 +1,12 @@
+'use strict'   // all the code wil be treated as per the newer version of javascript
+
+//When we use alert in node JS and perform arithmetic operation it will throw error because the JS engine (V8) is inside the browser and hence it will not give answer
+
+//1. Integer and all    -> typeof  	Number
+//2.  “anything”           -> typeof 	 string
+//3. True/ False           ->  typeof	  Boolean
+//4. NULL                      -> typeof 	 object
+//5. Undefined            -> typeof 	 undefined
+
+
+
